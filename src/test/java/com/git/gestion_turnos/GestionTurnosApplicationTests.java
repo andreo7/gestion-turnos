35d32596641eb73bcb41347c5159984a544dbe10
andreo7 <andreobruno3@gmail.com>
2026-01-26 16:07:08 -0300

    proyecto inicializado con Spring Boot

---
+++ src/test/java/com/git/gestion_turnos/GestionTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.gestion_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
