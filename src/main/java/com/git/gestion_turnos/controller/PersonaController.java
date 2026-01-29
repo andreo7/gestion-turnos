68d0a8755167e4f59e44bdd16057dada87822ba3
teivan1 <ivanchuas2012@gmail.com>
2026-01-29 16:34:27 -0300

    Primer Endpoint

--- src/main/java/com/git/gestion_turnos/controller/PersonaController.java
+++ src/main/java/com/git/gestion_turnos/controller/PersonaController.java
@@ -1,5 +1,23 @@
 package com.git.gestion_turnos.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.git.gestion_turnos.dto.PersonaDTO;
+import com.git.gestion_turnos.service.IPersona;
+
+@RestController
 public class PersonaController {
 
+    private IPersona iPersona;
+
+    public PersonaController(IPersona iPersona){
+        this.iPersona = iPersona;
+    }
+
+    @PostMapping
+    public PersonaDTO save(@RequestBody PersonaDTO persona){
+        return iPersona.save(persona);
+    }
 }
