5735aaa38f19a72074313f79e4b88af17ff8e209
andreo7 <andreobruno3@gmail.com>
2026-03-02 07:56:11 -0300

    feat: tests para el service de consulta de turno

--- src/test/java/com/git/gestion_turnos/service/TurnoServiceConsultaTest.java
+++ src/test/java/com/git/gestion_turnos/service/TurnoServiceConsultaTest.java
@@ -44,20 +44,34 @@ public class TurnoServiceConsultaTest {
     @DisplayName("Debe retornar el turno cuando existe el ID")
     public void obtenerTurno_cuandoExisteId(){
 
-        //ARRAGE - Arranque: preparo los datos con los que voy a testear
+        /**
+        // ARRANGE (Preparar): Creo los objetos necesarios para simular el escenario del test.
+        // En este caso, un Turno con id 1 y una Persona asociada. */
+
         Turno turnoObtenido = new Turno();
         turnoObtenido.setId(1);
         Persona personaTurno = new Persona();
         personaTurno.setNombre("Juan");
         turnoObtenido.setPersona(personaTurno);
 
-        //Esto le dice a mock: cuando llame a findById con el id 1 devolve el objeto turnoObtenido.
+        /** Configuro el comportamiento del mock:
+         * Cuando el repositorio reciba findById(1),
+         * debe devolver Optional.of(turnoObtenido). */
         when(turnoRepository.findById(1)).thenReturn(Optional.of(turnoObtenido));
 
+        /** ACT (Ejecutar)
+         * Llamo al método real del servicio que quiero probar.
+         * Este método internamente usará el mock configurado arriba. */
         TurnoDTO resultado = turnoServiceConsulta.findById(1);
 
+        // ASSERT (Verificar)
+        // Verifico que el resultado no sea nulo
         assertNotNull(resultado);
+
+        // Verifico que el nombre del cliente sea el esperado
         assertEquals("Juan", resultado.getCliente().getNombre());
+
+        // Verifico que el repositorio haya sido llamado con el id correcto
         verify(turnoRepository).findById(1);
     }
 
@@ -118,5 +132,34 @@ public class TurnoServiceConsultaTest {
 
         assertEquals(2, resultado.size());
         verify(turnoRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Debe retornar la lista con los turnos ocupados")
+    public void debeRetornarListaConTurnosOcupados(){
+        Turno turno1 = new Turno();
+        turno1.setId(1);
+        turno1.setEstado(EstadoTurno.DISPONIBLE);
+
+        Turno turno2 = new Turno();
+        turno2.setId(2);
+        turno2.setEstado(EstadoTurno.CONFIRMADO);
+
+        Turno turno3 = new Turno();
+        turno3.setId(3);
+        turno3.setEstado(EstadoTurno.RESERVADO);
+        Persona persona3 = new Persona();
+        persona3.setNombre("Juan");
+        turno3.setPersona(persona3);
+        List<Turno> turnosObtenidos = List.of(turno1, turno2, turno3);
+
+        when(turnoRepository.findAll()).thenReturn(turnosObtenidos);
+
+        List<TurnoDTO> resultado = turnoServiceConsulta.verTurnosOcupados();
+
+        assertEquals(2, resultado.size());
+        assertEquals("Juan", resultado.get(1).getCliente().getNombre());
+        assertEquals(EstadoTurno.CONFIRMADO, resultado.getFirst().getEstado());
+        verify(turnoRepository).findAll();
     }
 }
